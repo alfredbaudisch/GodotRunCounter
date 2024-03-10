ef18c4d9b7f0b653e85be929c7b0797d76f69b95
Alfred Reinold Baudisch <alfred@alfredbaudisch.com>
2024-03-10 09:33:46 +0000

    Increase counter in EditorPlugin._build instead of relying on an autoloader. Also, made file operations more robust, avoiding some deadlocks scenarios.

--- addons/RunCounter/plugin.gd
+++ addons/RunCounter/plugin.gd
@@ -1,8 +1,43 @@
 @tool
 extends EditorPlugin
 
+const DESTINATION = "user://"
+const FILE_NAME = "RunCounter.txt"
 
-func _init():
+var _file_path : String
+var _runs : int
+
+
+func _init() -> void:
 	name = "RunCounterPlugin"
-	add_autoload_singleton("RunCounter", "Scripts/RunCounter.gd")
+	_file_path = DESTINATION + FILE_NAME
+
+
+func _enter_tree() -> void:
+	_runs = 0
+	_save_to_file(true)
+
+
+func _build() -> bool:
+	_runs += 1
+	_save_to_file()
+	return true
+
+
+func _save_to_file(is_init := false) -> void:
+	var file := FileAccess.open(_file_path, FileAccess.READ_WRITE)
 	
+	# File does not exist, create it
+	if not file:
+		file = FileAccess.open(_file_path, FileAccess.WRITE_READ)
+	
+	# Get existing run count
+	elif file and is_init:
+		_runs = file.get_as_text().to_int()
+		file.close()
+		return
+
+	if file:
+		file.store_string(str(_runs))
+		file.close()
+
